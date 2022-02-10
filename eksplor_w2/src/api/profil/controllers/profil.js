b1c98c27263d2f62bd5f489b24f35e41fa5ed37b
risnasuci09 <80729529+risnasuci09@users.noreply.github.com>
2022-02-10 13:45:07 +0700

    Delete eksplor_w2 directory

--- eksplor_w2/src/api/profil/controllers/profil.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  profil controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::profil.profil');
