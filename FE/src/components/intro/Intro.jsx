938d293fdbd1fbb37568543b7f42cd0e037b64fb
DiwanPurnama <diwan.purnama.tif20@polban.ac.id>
2022-02-17 21:06:53 +0700

    update FE

---
+++ FE/src/components/intro/Intro.jsx
@@ -0,0 +1,14 @@
+import "./intro.scss"
+
+export default function Intro(){
+    return(
+        <div className="intro" id="intro">
+           <div className="left">
+               <div className="imageContainer">
+                   <img src="assets/tim.png" alt="" />
+               </div>
+           </div>
+           <div className="right"></div>
+        </div>
+    )
+}
