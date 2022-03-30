4a1a5cc7b6ce13095dbfe039f4f64741e03f8525
risnasuci09 <80729529+risnasuci09@users.noreply.github.com>
2022-03-30 08:45:28 +0700

    Delete FE directory

--- FE/web-profile-kelompok/src/components/index.js
+++
@@ -1,6 +0,0 @@
-import NavbarComponent from "./NavbarComponent";
-import ListRoles from "./ListRoles";
-import Hasil from "./Hasil";
-import Mahasiswas from "./Mahasiswas";
-
-export { NavbarComponent, ListRoles, Hasil, Mahasiswas }
