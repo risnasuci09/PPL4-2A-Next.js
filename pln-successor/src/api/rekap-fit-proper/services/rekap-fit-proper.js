e7dcfe73b0bc52c821472b848671d63b0694569c
risnasuci09 <risna.suci.tif20@polban.ac.id>
2022-03-30 08:32:11 +0700

    add desain database

---
+++ pln-successor/src/api/rekap-fit-proper/services/rekap-fit-proper.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * rekap-fit-proper service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::rekap-fit-proper.rekap-fit-proper');
