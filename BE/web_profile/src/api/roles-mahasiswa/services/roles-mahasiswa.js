47bc7deb1e763f29a7f5a3fdf29d96b573e10327
risnasuci09 <80729529+risnasuci09@users.noreply.github.com>
2022-03-30 08:45:10 +0700

    Delete BE directory

--- BE/web_profile/src/api/roles-mahasiswa/services/roles-mahasiswa.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * roles-mahasiswa service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::roles-mahasiswa.roles-mahasiswa');
