5923f663f10f62c43a47f218d8de66236d46c0d1
risnasuci09 <80729529+risnasuci09@users.noreply.github.com>
2022-03-30 08:44:19 +0700

    Delete profile-kelompok directory

--- profile-kelompok/config/admin.js
+++
@@ -1,5 +0,0 @@
-module.exports = ({ env }) => ({
-  auth: {
-    secret: env('ADMIN_JWT_SECRET', 'a50415ccdabf4126e4d4e2941dcccf55'),
-  },
-});
--- profile-kelompok/src/api/role-proyek/routes/role-proyek.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * role-proyek router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::role-proyek.role-proyek');
