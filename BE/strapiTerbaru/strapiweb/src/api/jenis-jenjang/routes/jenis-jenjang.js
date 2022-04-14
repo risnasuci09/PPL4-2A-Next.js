004eb03da47a33339b0ab7a329cacf3e2f669bc4
DiwanPurnama <diwan.purnama.tif20@polban.ac.id>
2022-04-14 22:39:00 +0700

    add project strapi terbaru

---
+++ BE/strapiTerbaru/strapiweb/src/api/jenis-jenjang/routes/jenis-jenjang.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * jenis-jenjang router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::jenis-jenjang.jenis-jenjang');
---
+++ BE/strapiTerbaru/strapiweb/src/api/nilai-key-competency/routes/nilai-key-competency.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * nilai-key-competency router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::nilai-key-competency.nilai-key-competency');
